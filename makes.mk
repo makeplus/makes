b5f6f5b43440b05eecdcbc0a7b6192177e162e82
Ingy döt Net <ingy@ingy.net>
2025-06-08 16:11:31 -0400

    Change $(args) to $_

--- makes.mk
+++ makes.mk
@@ -9,28 +9,28 @@ _makes-edit:
 	vim $(MAKES)/$(file)
 
 _makes-git:
-	git -C $(MAKES) $(args)
+	git -C $(MAKES) $_
 
 _makes-gll:
-	git -C $(MAKES) log --graph --decorate --pretty=oneline --abbrev-commit $(args)
+	git -C $(MAKES) log --graph --decorate --pretty=oneline --abbrev-commit $_
 
 _makes-head:
 	git -C $(MAKES) rev-parse HEAD
 
 _makes-status:
-	git -C $(MAKES) status --ignored $(args)
+	git -C $(MAKES) status --ignored $_
 
 _makes-branch _makes-diff _makes-fetch _makes-pull:
-	git -C $(MAKES) $(@:_makes-%=%) $(args)
+	git -C $(MAKES) $(@:_makes-%=%) $_
 
 _makes-add:
 	git -C $(MAKES) add -A .
 
 _makes-commit:
-	git -C $(MAKES) commit -av $(args)
+	git -C $(MAKES) commit -av $_
 
 _makes-push:
-	git -C $(MAKES) push git@github.com:makeplus/makes $(args) && \
+	git -C $(MAKES) push git@github.com:makeplus/makes $_ && \
 	  git -C $(MAKES) fetch
 
 _makes-env:
