28b825c07c91e504a132372de0141bfcdabd2101
Ingy döt Net <ingy@ingy.net>
2025-06-08 16:18:01 -0400

    Chamge $_ to $A

--- makes.mk
+++ makes.mk
@@ -9,28 +9,28 @@ _makes-edit:
 	vim $(MAKES)/$(file)
 
 _makes-git:
-	git -C $(MAKES) $_
+	git -C $(MAKES) $A
 
 _makes-gll:
-	git -C $(MAKES) log --graph --decorate --pretty=oneline --abbrev-commit $_
+	git -C $(MAKES) log --graph --decorate --pretty=oneline --abbrev-commit $A
 
 _makes-head:
 	git -C $(MAKES) rev-parse HEAD
 
 _makes-status:
-	git -C $(MAKES) status --ignored $_
+	git -C $(MAKES) status --ignored $A
 
 _makes-branch _makes-diff _makes-fetch _makes-pull:
-	git -C $(MAKES) $(@:_makes-%=%) $_
+	git -C $(MAKES) $(@:_makes-%=%) $A
 
 _makes-add:
 	git -C $(MAKES) add -A .
 
 _makes-commit:
-	git -C $(MAKES) commit -av $_
+	git -C $(MAKES) commit -av $A
 
 _makes-push:
-	git -C $(MAKES) push git@github.com:makeplus/makes $_ && \
+	git -C $(MAKES) push git@github.com:makeplus/makes $A && \
 	  git -C $(MAKES) fetch
 
 _makes-env:
