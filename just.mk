ee76b79a32aabfb041c31723c89e7957fa487f54
Ingy döt Net <ingy@ingy.net>
2025-06-08 22:59:33 -0400

    Add just.mk

---
+++ just.mk
@@ -0,0 +1,37 @@
+ifndef MAKES
+$(error Please 'include .makes/init.mk')
+endif
+ifndef LOCAL-ROOT
+include $(MAKES)/local.mk
+endif
+
+JUST-VERSION ?= 1.40.0
+
+JUST-TARBALL := just-$(JUST-VERSION)-x86_64-unknown-linux-musl.tar.gz
+JUST-DOWNLOAD := https://github.com/casey/just/releases/download/$(JUST-VERSION)/$(JUST-TARBALL)
+
+JUST := $(LOCAL-BIN)/just
+
+JUST-CMDS := \
+  bench \
+  check \
+  fmt-check \
+  build \
+  clean \
+  fuzz \
+  test-unit \
+  clean-rust \
+  clippy \
+  guests \
+  test \
+  test-isolated \
+
+$(JUST):: $(LOCAL-CACHE)/$(JUST-TARBALL)
+	tar -C $(LOCAL-BIN) -xf $< just
+	[[ -e $@ ]]
+	touch $@
+	@echo
+
+$(LOCAL-CACHE)/$(JUST-TARBALL):
+	@echo "Installing 'go' locally"
+	curl -Ls $(JUST-DOWNLOAD) > $@
