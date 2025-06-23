4e5f204e6925822718486e003d5b240cc1ec3261
Ingy döt Net <ingy@ingy.net>
2025-06-23 11:11:15 -0400

    Add shell.mk

---
+++ shell.mk
@@ -0,0 +1,10 @@
+shell:
+ifndef MAKES_SHELL
+	@( \
+	  export MAKES_SHELL=1; \
+	  bash --rcfile \
+	  <(cat ~/.bashrc; echo 'PS1="(makes) \w $$ "') \
+	)
+else
+	@echo 'You are already in a Makes bash shell'
+endif
