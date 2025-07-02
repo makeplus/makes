6bca920e0b9cefe599218ab1923927beb099e286
Ingy döt Net <ingy@ingy.net>
2025-07-02 09:43:36 -0700

    Add MAKES-SHELL-NAME, MAKES-SHELL-DEPS

--- shell.mk
+++ shell.mk
@@ -3,12 +3,18 @@ $(error shell.mk already loaded)
 endif
 SHELL-LOADED := true
 
-shell:
+MAKES-SHELL-NAME ?= makes
+
+
+shell: $(MAKES-SHELL-DEPS)
 ifndef MAKES_SHELL
 	@( \
 	  export MAKES_SHELL=1; \
 	  bash --rcfile \
-	  <(cat ~/.bashrc; echo 'PS1="(makes) \w $$ "') \
+	  <(cat ~/.bashrc; \
+	    echo 'PS1="($(MAKES-SHELL-NAME)) \w $$ "'; \
+	    echo 'export PATH="$(PATH)"'; \
+	   ) \
 	)
 else
 	@echo 'You are already in a Makes bash shell'
