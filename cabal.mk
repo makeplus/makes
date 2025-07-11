1c92cdf2fcbc6cf9e59e044f7c6d1fde2101d65f
Ingy döt Net <ingy@ingy.net>
2025-07-11 10:21:32 -0700

    Add cabal.mk

---
+++ cabal.mk
@@ -0,0 +1,32 @@
+CABAL-VERSION ?= 3.14.2.0
+
+ifndef CABAL-LOADED
+CABAL-LOADED := true
+
+$(if $(MAKES),,$(error Please 'include .makes/init.mk'))
+$(eval $(call include-local))
+include ghc.mk
+
+OA-linux-arm64 := aarch64-linux-deb12
+OA-linux-int64 := x86_64-linux-ubuntu22_04
+OA-macos-arm64 := aarch64-darwin
+OA-macos-int64 := x86_64-darwin
+
+CABAL-TARBALL := cabal-install-$(CABAL-VERSION)-$(OA-$(OS-ARCH)).tar.xz
+CABAL-DOWNLOAD := https://downloads.haskell.org/~cabal/cabal-install-$(CABAL-VERSION)/$(CABAL-TARBALL)
+
+CABAL := $(LOCAL-BIN)/cabal
+
+SHELL-DEPS += $(CABAL)
+
+
+$(CABAL): $(LOCAL-CACHE)/$(CABAL-TARBALL)
+	tar -C $(LOCAL-BIN) -xf $< cabal
+	touch $@
+	@echo
+
+$(LOCAL-CACHE)/$(CABAL-TARBALL):
+	@echo "Installing 'Cabal $(CABAL-VERSION)' locally"
+	curl+ $(CABAL-DOWNLOAD) > $@
+
+endif 
