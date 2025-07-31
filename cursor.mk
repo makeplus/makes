bbd1437e628df83d1b2ce829c03126d487f43996
Ingy döt Net <ingy@ingy.net>
2025-07-30 18:55:35 -0700

    Fix cursor.mk bugs

--- cursor.mk
+++ cursor.mk
@@ -10,10 +10,9 @@ endif
 CURSOR-HISTORY-URL := \
   https://github.com/oslook/cursor-ai-downloads/raw/HEAD/version-history.json
 
-CURSOR-HISTORY-JSON := $(shell curl -sL $(CURSOR-HISTORY-URL))
-
 CURSOR-VERSION ?= $(shell \
-  jq -r '.versions[0].version' <<<'$(CURSOR-HISTORY-JSON)')
+   curl -sL $(CURSOR-HISTORY-URL) | \
+   jq -r '.versions[0].version')
 
 OA-linux-arm64 := linux-arm64
 OA-linux-int64 := linux-amd64
@@ -22,14 +21,17 @@ OA-macos-int64 := darwin-amd64
 
 ifdef CURSOR-VERSION
   CURSOR-DATE := $(shell \
-    jq -r '.versions[0].date' <<<'$(CURSOR-HISTORY-JSON)')
+    curl -sL $(CURSOR-HISTORY-URL) | jq -r '.versions[0].date')
   URL-linux-int64 := $(shell \
-    jq -r '.versions[] | select(.version == "$(CURSOR-VERSION)") | \
-    .platforms."linux-x64"' <<<'$(CURSOR-HISTORY-JSON)')
+    curl -sL $(CURSOR-HISTORY-URL) | \
+    jq -r '.versions[] | \
+      select(.version == "$(CURSOR-VERSION)") | \
+      .platforms."linux-x64"')
 else
   CURSOR-VERSION := 1.2.2
   CURSOR-DATE := 2025-07-07
-  URL-linux-int64 := https://downloads.cursor.com/production/faa03b17cce93e8a80b7d62d57f5eda6bb6ab9fa/linux/x64/Cursor-1.2.2-x86_64.AppImage
+  URL-linux-int64 := \
+    https://downloads.cursor.com/production/faa03b17cce93e8a80b7d62d57f5eda6bb6ab9fa/linux/x64/Cursor-1.2.2-x86_64.AppImage
 endif
 
 CURSOR-DOWN := $(URL-$(OS-ARCH))
