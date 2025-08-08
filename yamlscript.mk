e0eb12e45ef77860629a56739d592c7ee5f7faf3
Ingy döt Net <ingy@ingy.net>
2025-08-08 11:21:14 -0700

    Add yamlscript.mk

---
+++ yamlscript.mk
@@ -0,0 +1,34 @@
+YAMLSCRIPT-VERSION ?= 0.2.1
+
+ifndef YAMLSCRIPT-LOADED
+YAMLSCRIPT-LOADED := true
+$(if $(MAKES),,$(error Please 'include init.mk' first))
+$(eval $(call include-local))
+
+OA-linux-arm64 := linux-aarch64
+OA-linux-int64 := linux-x64
+OA-macos-arm64 := macos-aarch64
+OA-macos-int64 := macos-x64
+
+YAMLSCRIPT-DIR := ys-$(YAMLSCRIPT-VERSION)-$(OA-$(OS-ARCH))
+YAMLSCRIPT-TAR := $(YAMLSCRIPT-DIR).tar.xz
+YAMLSCRIPT-DOWN := https://github.com/yaml/yamlscript
+YAMLSCRIPT-DOWN := $(YAMLSCRIPT-DOWN)/releases/download/$(YAMLSCRIPT-VERSION)/$(YAMLSCRIPT-TAR)
+
+YAMLSCRIPT := $(LOCAL-BIN)/ys-$(YAMLSCRIPT-VERSION)
+
+SHELL-DEPS += $(YS)
+
+
+$(YS): $(LOCAL-CACHE)/$(YAMLSCRIPT-TAR)
+	tar -C $(LOCAL-CACHE) -xf $<
+	[[ -e $(LOCAL-CACHE)/$(YAMLSCRIPT-DIR)/ys-$(YAMLSCRIPT-VERSION) ]]
+	mv $(LOCAL-CACHE)/$(YAMLSCRIPT-DIR)/ys* $(LOCAL-BIN)
+	touch $@
+	@echo
+
+$(LOCAL-CACHE)/$(YAMLSCRIPT-TAR):
+	@echo "Installing 'ys' locally"
+	curl+ $(YAMLSCRIPT-DOWN) > $@
+
+endif
