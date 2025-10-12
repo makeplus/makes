8ff6ffce8176d7d465b63e1f0b9b5764dc534602
Ingy döt Net <ingy@ingy.net>
2025-10-12 10:14:33 -0700

    Add buf.mk

---
+++ buf.mk
@@ -0,0 +1,34 @@
+# https://github.com/bufbuild/buf/releases
+BUF-VERSION ?= 1.18.0
+
+ifndef BUF-LOADED
+BUF-LOADED := true
+$(if $(MAKES),,$(error Please 'include init.mk' first))
+$(eval $(call include-local))
+
+OA-linux-arm64 := Linux-aarch64
+OA-linux-int64 := Linux-x86_64
+OA-macos-arm64 := Darwin-arm64
+OA-macos-int64 := Darwin-x86_64
+
+BUF-DIR := buf-$(OA-$(OS-ARCH))
+BUF-TAR := $(BUF-DIR).tar.gz
+BUF-DOWN := https://github.com/bufbuild/buf
+BUF-DOWN := $(BUF-DOWN)/releases/download/v$(BUF-VERSION)/$(BUF-TAR)
+
+BUF := $(LOCAL-BIN)/buf
+
+SHELL-DEPS += $(BUF)
+
+
+$(BUF): $(LOCAL-CACHE)/$(BUF-TAR)
+	$Q tar -C $(LOCAL-CACHE) -xf $<
+	cp -r $(LOCAL-CACHE)/buf/{bin,etc,share} $(LOCAL-PREFIX)
+	[[ -e $@ ]]
+	@$(ECHO)
+
+$(LOCAL-CACHE)/$(BUF-TAR):
+	@$(ECHO) "* Installing 'buf' locally"
+	$Q curl+ $(BUF-DOWN) > $@
+
+endif
