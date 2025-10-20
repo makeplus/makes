e7a9c66fadfeee3e544fe393bbab55e55ec1119e
Ingy döt Net <ingy@ingy.net>
2025-10-20 15:09:53 -0700

    Add libys.mk

---
+++ libys.mk
@@ -0,0 +1,33 @@
+LIBYS-VERSION ?= 0.2.7
+
+ifndef LIBYS-LOADED
+LIBYS-LOADED := true
+$(if $(MAKES),,$(error Please 'include init.mk' first))
+$(eval $(call include-local))
+
+OA-linux-arm64 := linux-aarch64
+OA-linux-int64 := linux-x64
+OA-macos-arm64 := macos-aarch64
+OA-macos-int64 := macos-x64
+
+LIBYS-DIR := libys-$(LIBYS-VERSION)-$(OA-$(OS-ARCH))
+LIBYS-TAR := $(LIBYS-DIR).tar.xz
+LIBYS-DOWN := https://github.com/yaml/yamlscript
+LIBYS-DOWN := $(LIBYS-DOWN)/releases/download/$(LIBYS-VERSION)/$(LIBYS-TAR)
+
+LIBYS := $(LOCAL-LIB)/libys.$(SO).$(LIBYS-VERSION)
+
+SHELL-DEPS += $(LIBYS)
+
+
+$(LIBYS): $(LOCAL-CACHE)/$(LIBYS-TAR)
+	tar -C $(LOCAL-CACHE) -xf $<
+	$(MAKE) -C $(LOCAL-CACHE)/$(LIBYS-DIR) install PREFIX=$(LOCAL-PREFIX)
+	touch $@
+	@echo
+
+$(LOCAL-CACHE)/$(LIBYS-TAR):
+	@echo "* Installing 'libys' locally"
+	curl+ $(LIBYS-DOWN) > $@
+
+endif
