c988d951df5ffad92af1f5d1904925581dd9cc5e
Ingy döt Net <ingy@ingy.net>
2025-12-24 15:43:40 -0800

    Add helm.mk

---
+++ helm.mk
@@ -0,0 +1,54 @@
+HELM-VERSION ?= 3.17.0
+# https://github.com/helm/helm
+
+ifndef HELM-LOADED
+HELM-LOADED := true
+$(if $(MAKES),,$(error Please 'include init.mk' first))
+$(eval $(call include-local))
+
+#------------------------------------------------------------------------------
+# OS/Architecture Mappings
+#------------------------------------------------------------------------------
+
+# helm uses standard linux/darwin with amd64/arm64
+OA-linux-arm64 := linux-arm64
+OA-linux-int64 := linux-amd64
+OA-macos-arm64 := darwin-arm64
+OA-macos-int64 := darwin-amd64
+
+#------------------------------------------------------------------------------
+# Download Configuration
+#------------------------------------------------------------------------------
+
+HELM-TAR := helm-v$(HELM-VERSION)-$(OA-$(OS-ARCH)).tar.gz
+HELM-DOWN := https://get.helm.sh/$(HELM-TAR)
+
+#------------------------------------------------------------------------------
+# Local Paths
+#------------------------------------------------------------------------------
+
+HELM := $(LOCAL-BIN)/helm
+
+#------------------------------------------------------------------------------
+# Shell Dependencies
+#------------------------------------------------------------------------------
+
+SHELL-DEPS += $(HELM)
+
+#------------------------------------------------------------------------------
+# Binary Installation Target
+#------------------------------------------------------------------------------
+
+$(HELM): $(LOCAL-CACHE)/$(HELM-TAR)
+	tar -C $(LOCAL-CACHE) -xzf $<
+	cp $(LOCAL-CACHE)/$(OA-$(OS-ARCH))/helm $@
+	rm -rf $(LOCAL-CACHE)/$(OA-$(OS-ARCH))
+	chmod +x $@
+	touch $@
+	@echo
+
+$(LOCAL-CACHE)/$(HELM-TAR):
+	@echo "* Installing 'helm' locally"
+	curl+ $(HELM-DOWN) > $@
+
+endif
