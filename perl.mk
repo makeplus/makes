7d5ffe744eaf7d77948344c7696792229321ce3d
Ingy döt Net <ingy@ingy.net>
2026-04-01 23:36:05 -0700

    Fix and enhance perl.mk

--- perl.mk
+++ perl.mk
@@ -11,9 +11,9 @@ OA-macos-arm64 := darwin-arm64
 OA-macos-int64 := darwin-amd64
 
 PERL-DIR := perl-$(OA-$(OS-ARCH))
-PERL-TAR := $(PERL-DIR).tar.gz
+PERL-TAR := perl-$(PERL-VERSION)-$(OA-$(OS-ARCH)).tar.gz
 PERL-DOWN := https://github.com/skaji/relocatable-perl/releases/download
-PERL-DOWN := $(PERL-DOWN)/$(PERL-VERSION)/$(PERL-TAR)
+PERL-DOWN := $(PERL-DOWN)/$(PERL-VERSION)/$(PERL-DIR).tar.gz
 
 ifeq ($(OS-NAME),windows)
 # Use system Perl on Windows (GitHub Actions runners have Strawberry Perl)
@@ -30,19 +30,51 @@ PERL-BIN := $(PERL-LOCAL)/bin
 override PATH := $(PERL-BIN):$(PATH)
 export PATH
 
+PERL_CPANM_HOME := $(LOCAL-ROOT)/cpanm
+export PERL_CPANM_HOME
+
+PERL-CPANFILE-DEPS := $(PERL-LOCAL)/.cpan-deps-installed
+
+
 PERL := $(LOCAL-BIN)/perl
+PERL-CPAN-DEPS := $(PERL-LOCAL)/.cpan-deps-installed
+PERL-LINK := $(LOCAL-BIN)/.perl-$(PERL-VERSION)
 
 SHELL-DEPS += $(PERL)
 
-$(PERL): $(LOCAL-CACHE)/$(PERL-TAR)
+$(PERL): $(PERL-LINK)
+
+$(PERL-LINK): $(PERL-LOCAL)/bin/perl
+	rm -f $(LOCAL-BIN)/.perl-*
+	ln -sf $< $(PERL)
+	touch $@
+
+$(PERL-LOCAL)/bin/perl: $(LOCAL-CACHE)/$(PERL-TAR)
 	tar -C $(LOCAL-CACHE) -xzf $<
+	rm -rf $(PERL-LOCAL)
 	mv $(LOCAL-CACHE)/$(PERL-DIR) $(PERL-LOCAL)
 	touch $@
 	@echo
-endif
 
 $(LOCAL-CACHE)/$(PERL-TAR):
 	@echo "* Installing 'perl' locally"
 	curl+ $(PERL-DOWN) > $@
+
+ifneq (,$(wildcard Makefile.PL))
+Makefile: Makefile.PL $(PERL-LINK)
+	perl $<
+endif
+
+$(PERL-CPANFILE-DEPS): Makefile.PL cpanfile $(PERL)
+	cpanm -n --installdeps .
+	touch $@
+
+endif
+
+PERL-RELEASES-URL := https://api.github.com/repos/skaji/relocatable-perl/releases
+perl-versions:
+	@curl -sL '$(PERL-RELEASES-URL)?per_page=100' | \
+	grep '"tag_name"' | \
+	sed 's/.*: "//;s/".*//'
 
 endif
