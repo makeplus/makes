eb61551e5d87a4726eee7dda0e29c3f51401db7f
Ingy döt Net <ingy@ingy.net>
2026-05-23 10:32:58 -0400

    Add hy.mk

---
+++ hy.mk
@@ -0,0 +1,28 @@
+HY-VERSION ?= 1.2.0
+# https://github.com/hylang/hy
+
+ifndef HY-LOADED
+HY-LOADED := true
+$(if $(MAKES),,$(error Please 'include init.mk' first))
+include $(MAKES)/uv.mk
+
+HY-LOCAL := $(LOCAL-ROOT)/hy-$(HY-VERSION)
+HY := $(HY-LOCAL)/bin/hy
+
+export UV_TOOL_DIR := $(HY-LOCAL)/tools
+export UV_TOOL_BIN_DIR := $(HY-LOCAL)/bin
+export UV_PYTHON_INSTALL_DIR := $(LOCAL-ROOT)
+
+override PATH := $(HY-LOCAL)/bin:$(PATH)
+export PATH
+
+SHELL-DEPS += $(HY)
+
+
+$(HY): $(UV)
+	@$(ECHO) "* Installing 'hy' locally"
+	$Q uv tool install hy==$(HY-VERSION)
+	$Q touch $@
+	@$(ECHO)
+
+endif
