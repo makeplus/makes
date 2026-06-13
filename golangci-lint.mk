f83d9b36c80f97bcb1e3ac46678907d636b0438f
Ingy döt Net <ingy@ingy.net>
2026-06-13 15:45:06 -0400

    Update golangci-lint to v2

--- golangci-lint.mk
+++ golangci-lint.mk
@@ -1,4 +1,4 @@
-GOLANGCI-LINT-VERSION ?= 1.64.8
+GOLANGCI-LINT-VERSION ?= 2.12.2
 # https://github.com/golangci/golangci-lint
 
 ifndef GOLANGCI-LINT-LOADED
@@ -6,14 +6,22 @@ GOLANGCI-LINT-LOADED := true
 $(if $(MAKES),,$(error Please 'include init.mk' first))
 include $(MAKES)/go.mk
 
-GOLANGCI-LINT-PKG := github.com/golangci/golangci-lint/cmd/golangci-lint@v$(GOLANGCI-LINT-VERSION)
+GOLANGCI-LINT-PKG := github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v$(GOLANGCI-LINT-VERSION)
+GOLANGCI-LINT-LOCAL := $(LOCAL-ROOT)/golangci-lint-$(GOLANGCI-LINT-VERSION)
+GOLANGCI-LINT-BIN := $(GOLANGCI-LINT-LOCAL)/bin/golangci-lint
 GOLANGCI-LINT := $(LOCAL-BIN)/golangci-lint
 
 SHELL-DEPS += $(GOLANGCI-LINT)
 
-$(GOLANGCI-LINT): $(GO)
+$(GOLANGCI-LINT): $(GOLANGCI-LINT-BIN)
+	$Q rm -f $@
+	$Q ln -s $< $@
+	@$(ECHO)
+
+$(GOLANGCI-LINT-BIN): $(GO)
 	@$(ECHO) "* Installing 'golangci-lint' locally"
-	$Q GOBIN=$(LOCAL-BIN) go install $(GOLANGCI-LINT-PKG) $O
+	$Q mkdir -p $(dir $@)
+	$Q GOBIN=$(dir $@) go install $(GOLANGCI-LINT-PKG) $O
 	$Q touch $@
 	@$(ECHO)
 
