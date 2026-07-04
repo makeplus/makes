c34cb3f8259645ac9f013774ab919cce903dc655
Ingy döt Net <ingy@ingy.net>
2026-07-04 16:21:32 -0400

    Add macOS support to swift.mk
    swiftly for macOS is distributed as a universal pkg under
    /swiftly/darwin/, not a Linux-style tarball. Download the pkg and
    extract the swiftly binary locally with pkgutil --expand-full, so
    nothing is installed outside the Makes local directory. Fixes a 404
    when installing Swift on macOS.

--- swift.mk
+++ swift.mk
@@ -12,8 +12,14 @@ OA-linux-int64 := x86_64
 SWIFTLY-HOME := $(LOCAL-ROOT)/swiftly
 SWIFTLY := $(SWIFTLY-HOME)/bin/swiftly
 SWIFT := $(SWIFTLY-HOME)/bin/swift
-SWIFTLY-TAR := swiftly-$(OA-$(OS-ARCH)).tar.gz
-SWIFTLY-DOWN := https://download.swift.org/swiftly/linux/$(SWIFTLY-TAR)
+ifeq ($(OS-NAME),macos)
+# macOS swiftly is a universal pkg (no arch in the name):
+SWIFTLY-ARCHIVE := swiftly.pkg
+SWIFTLY-DOWN := https://download.swift.org/swiftly/darwin/$(SWIFTLY-ARCHIVE)
+else
+SWIFTLY-ARCHIVE := swiftly-$(OA-$(OS-ARCH)).tar.gz
+SWIFTLY-DOWN := https://download.swift.org/swiftly/linux/$(SWIFTLY-ARCHIVE)
+endif
 
 SHELL-DEPS += $(SWIFT)
 
@@ -26,12 +32,18 @@ export GNUPGHOME := $(SWIFTLY-HOME)/gnupg
 export CLANG_MODULE_CACHE_PATH := $(SWIFTLY-HOME)/clang-modules
 
 
-$(SWIFT): $(LOCAL-CACHE)/$(SWIFTLY-TAR)
+$(SWIFT): $(LOCAL-CACHE)/$(SWIFTLY-ARCHIVE)
 	$Q rm -rf $(SWIFTLY-HOME) $(LOCAL-TMP)/swiftly
 	$Q mkdir -p $(SWIFTLY-HOME)/bin $(LOCAL-TMP)/swiftly
 	$Q mkdir -p -m 700 $(GNUPGHOME)
+ifeq ($(OS-NAME),macos)
+	$Q pkgutil --expand-full $< $(LOCAL-TMP)/swiftly/pkg
+	$Q cp $$(find $(LOCAL-TMP)/swiftly/pkg -type f -name swiftly | \
+	    head -1) $(SWIFTLY)
+else
 	$Q tar -C $(LOCAL-TMP)/swiftly -xzf $<
 	$Q cp $(LOCAL-TMP)/swiftly/swiftly $(SWIFTLY)
+endif
 	$Q chmod +x $(SWIFTLY)
 	$Q $(SWIFTLY) init --assume-yes --no-modify-profile --skip-install --quiet-shell-followup
 	$Q . $(SWIFTLY-HOME)/env.sh && $(SWIFTLY) install --use $(SWIFT-VERSION) \
@@ -43,7 +55,7 @@ $(SWIFT): $(LOCAL-CACHE)/$(SWIFTLY-TAR)
 	$Q touch $@
 	@$(ECHO)
 
-$(LOCAL-CACHE)/$(SWIFTLY-TAR):
+$(LOCAL-CACHE)/$(SWIFTLY-ARCHIVE):
 	@$(ECHO) "* Installing 'swift' locally"
 	$Q curl+ $(SWIFTLY-DOWN) > $@
 
