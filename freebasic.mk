9e71893357059d695c2ac8c93596488b5c13b803
Ingy döt Net <ingy@ingy.net>
2026-07-06 14:40:20 -0400

    Fix FreeBASIC download path

--- freebasic.mk
+++ freebasic.mk
@@ -7,20 +7,22 @@ $(if $(MAKES),,$(error Please 'include init.mk' first))
 $(eval $(call include-local))
 
 OA-linux-arm64 := ubuntu-22.04-aarch64
-OA-linux-int64 := linux-x86_64
+OA-linux-int64 := ubuntu-22.04-x86_64
 OA-windows-int64 := win64
 
 $(if $(OA-$(OS-ARCH)),,$(error freebasic.mk does not support $(OS-ARCH)))
 
+FREEBASIC-LOCAL := $(LOCAL-ROOT)/freebasic-$(FREEBASIC-VERSION)
 ifeq ($(OS-NAME),windows)
 FREEBASIC-ARCHIVE := FreeBASIC-$(FREEBASIC-VERSION)-$(OA-$(OS-ARCH)).zip
+FREEBASIC-DIR := Binaries-Windows
 FREEBASIC := $(FREEBASIC-LOCAL)/fbc64.exe
 else
-FREEBASIC-ARCHIVE := FreeBASIC-$(FREEBASIC-VERSION)-$(OA-$(OS-ARCH)).tar.gz
+FREEBASIC-ARCHIVE := FreeBASIC-$(FREEBASIC-VERSION)-$(OA-$(OS-ARCH)).tar.xz
+FREEBASIC-DIR := Binaries-Linux
 FREEBASIC := $(FREEBASIC-LOCAL)/bin/fbc
 endif
-FREEBASIC-DOWN := https://sourceforge.net/projects/fbc/files/FreeBASIC-$(FREEBASIC-VERSION)/$(FREEBASIC-ARCHIVE)/download
-FREEBASIC-LOCAL := $(LOCAL-ROOT)/freebasic-$(FREEBASIC-VERSION)
+FREEBASIC-DOWN := https://sourceforge.net/projects/fbc/files/FreeBASIC-$(FREEBASIC-VERSION)/$(FREEBASIC-DIR)/$(FREEBASIC-ARCHIVE)/download
 
 SHELL-DEPS += $(FREEBASIC)
 
@@ -41,7 +43,7 @@ $(FREEBASIC): $(LOCAL-CACHE)/$(FREEBASIC-ARCHIVE)
 	@$(ECHO) "* Installing 'freebasic' locally"
 	$Q rm -rf $(FREEBASIC-LOCAL) $(LOCAL-TMP)/freebasic-$(FREEBASIC-VERSION)
 	$Q mkdir -p $(LOCAL-TMP)/freebasic-$(FREEBASIC-VERSION)
-	$Q tar -C $(LOCAL-TMP)/freebasic-$(FREEBASIC-VERSION) -xzf $<
+	$Q tar -C $(LOCAL-TMP)/freebasic-$(FREEBASIC-VERSION) -xJf $<
 	$Q mv $$(find $(LOCAL-TMP)/freebasic-$(FREEBASIC-VERSION) -mindepth 1 -maxdepth 1 -type d | head -1) $(FREEBASIC-LOCAL)
 	$Q touch $@
 	@$(ECHO)
