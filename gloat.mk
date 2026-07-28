e3157165cd1ee0a5e3eaca879b1015f4a7f5a407
Ingy döt Net <ingy@ingy.net>
2026-07-28 15:26:44 -0400

    Update versions

--- gloat.mk
+++ gloat.mk
@@ -1,4 +1,4 @@
-GLOAT-VERSION ?= 0.1.66
+GLOAT-VERSION ?= 0.1.67
 # https://github.com/gloathub/gloat
 
 ifndef GLOAT-LOADED
--- uv.mk
+++ uv.mk
@@ -1,4 +1,4 @@
-UV-VERSION ?= 0.11.33
+UV-VERSION ?= 0.12.0
 
 ifndef UV-LOADED
 UV-LOADED := true
