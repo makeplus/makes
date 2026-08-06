c853826dd72f9c67dc931f70f185742fa79d3be0
Ingy döt Net <ingy@ingy.net>
2026-08-06 13:20:44 -0400

    Update versions

--- gloat.mk
+++ gloat.mk
@@ -1,4 +1,4 @@
-GLOAT-VERSION ?= 0.1.73
+GLOAT-VERSION ?= 0.1.74
 # https://github.com/gloathub/gloat
 
 ifndef GLOAT-LOADED
--- jolt.mk
+++ jolt.mk
@@ -1,4 +1,4 @@
-JOLT-VERSION ?= 0.6.5
+JOLT-VERSION ?= 0.6.6
 # https://github.com/jolt-lang/jolt
 
 ifndef JOLT-LOADED
