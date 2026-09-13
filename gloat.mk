0078a645f45e18e4128e02eb607423dc51c16da7
Ingy döt Net <ingy@ingy.net>
2026-09-13 00:46:32 -0400

    Update versions

--- gloat.mk
+++ gloat.mk
@@ -1,4 +1,4 @@
-GLOAT-VERSION ?= 0.1.84
+GLOAT-VERSION ?= 0.1.85
 # https://github.com/gloathub/gloat
 
 ifndef GLOAT-LOADED
--- prolog.mk
+++ prolog.mk
@@ -1,4 +1,4 @@
-TREALLA-VERSION ?= 3.9.73
+TREALLA-VERSION ?= 3.9.74
 # https://github.com/trealla-prolog/trealla
 
 ifndef PROLOG-LOADED
